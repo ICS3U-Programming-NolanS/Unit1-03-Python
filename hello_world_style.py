b14170d8e8cc607cba9e9f665bf7277968ebdfd4
Nolan Shami <nolan.shami@stu.ocsb.ca>
2022-09-16 04:17:30 +0000

    updated

---
+++ hello_world_style.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Created By: Nolan Shami
+# Date: Sept, 15 2022
+This program displays Hello, World! with proper style.
+
+
+def main():
+print ("Hello, World! with proper style!")
+print("Completed in Python!")
+
+
+if __name__ == "__main__":
+main()
